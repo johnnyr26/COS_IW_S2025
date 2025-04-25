0d01e936659426be4b71d122afbd88c3c8651f4c
Johnny <ramirezjohnny@icloud.com>
2025-04-24 23:05:43 -0400

    Implement Web_Scraper class

---
+++ simulator/web_scraper.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+class WebsiteNotFoundException(Exception):
+    def __init__(self, message: str):
+        """
+        Exception raised for a website that does not have
+        the expected content.
+
+        :params message: the error message.
+        """
+        self.message = message
+        super().__init__(self.message)
+
+
+class Web_Scraper:
+    def scrape_wikipedia_article(self, url: str) -> dict[str, str]:
+        """
+        Scrapes a wikipedia article and returns the first paragraph
+        of the article.
+
+        :params url: the url of the Wikipedia article.
+        :returns: a dictionary with the url and the first paragraph
+        """
+        try:
+            response = requests.get(url, timeout=10)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            paragraphs = [p.get_text() for p in soup.find_all('p')]
+            if paragraphs[0] == "The requested page title is empty or contains only a namespace prefix.\n":
+                raise WebsiteNotFoundException(f"Wikipedia article url {url} is not found.")
+            # the first element of a valid article is always \n.
+            return {"url": url, "text": paragraphs[1]}
+        except WebsiteNotFoundException as ex:
+            return {"url": url, "error": ex.message}
+
+if __name__ == "__main__":
+    web_scraper = Web_Scraper()
+    print(web_scraper.scrape_wikipedia_article("https://en.wikipedia.org/?curid=59"))
